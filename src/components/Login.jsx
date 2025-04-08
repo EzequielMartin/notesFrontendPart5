fb2dcb969ecfb292aaa4e0a5cdb7280226b6e7a4
Ezequiel Martin <ezequielmartin92@gmail.com>
2025-04-07 21:54:47 -0300

    Hago algunos cambios que me ayudan en las pruebas e2e

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,10 +10,10 @@ const LoginForm = ({
         <h2>Login</h2>
         <form onSubmit={handleSubmit}>
           <div>
-            username <input type="text" value={username} name="Username" onChange={ handleUsernameChange } />
+            username <input data-testid="username" type="text" value={username} name="Username" onChange={ handleUsernameChange } />
           </div>
           <div>
-            password <input type="password" value={password} name="Password" onChange={ handlePasswordChange } />
+            password <input data-testid="password" type="password" value={password} name="Password" onChange={ handlePasswordChange } />
           </div>
           <button type="submit">login</button>
       </form>
